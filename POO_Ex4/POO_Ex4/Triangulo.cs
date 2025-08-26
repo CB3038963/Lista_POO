fb91b18b64a9627231bcb74a8afe220c820fbcbd
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-26 00:25:10 -0300

    Add files via upload

---
+++ POO_Ex4/POO_Ex4/Triangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex4
+{
+    internal class Triangulo
+    {
+        private int b, h, a;
+        //construtore
+        public Triangulo() { this.b = 0; this.h = 0; }
+        public Triangulo(int b,int h) { this.b = b;this.h = h; }
+
+        //set
+        public void SetBase(int b){ this.b = b; }
+        public void SetAltura(int h){ this.h = h; }
+        //get
+        public int GetBase(){ return this.b; }  
+        public int GetAltura() { return this.h; }
+        public int GetArea() { return this.a; }
+        //resultado
+        public void Calc() { this.a = this.b * this.h / 2; }
+
+    }
+}
