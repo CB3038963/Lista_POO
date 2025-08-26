2406317e948c037b52f43918845ddf8216a6442f
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-26 00:52:27 -0300

    Add files via upload

---
+++ POO_Ex5/POO_Ex5/Maritima.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex5
+{
+    internal class Maritima
+    {
+        private int maritima = 1852;
+        private int milhas;
+        private int km;
+        //construtores
+        public Maritima () { this.km = 0; }
+        public Maritima (int km) {  this.km = km; }
+        //set
+        public void SetKilo(int km) {  this.km=km; }
+        //Get
+        public int GetKilo() { return this.km; }
+        public int GetMilhas() { return this.milhas; }
+        //resultado
+        public void Converter() { this.milhas = this.km * this.maritima; }
+
+    }
+}
