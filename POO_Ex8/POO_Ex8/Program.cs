0385f0afadc560574c99573ddbbf840d7134bc86
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-26 02:04:44 -0300

    Add files via upload

---
+++ POO_Ex8/POO_Ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior maior = new Maior(100,10);
+            maior.Verificar();
+            Console.WriteLine(maior.GetMaior());
+        }
+    }
+}
