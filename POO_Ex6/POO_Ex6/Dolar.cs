b887732149c2959503a51e4566083af8caffe773
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-26 01:39:17 -0300

    Add files via upload

---
+++ POO_Ex6/POO_Ex6/Dolar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex6
+{
+    internal class Dolar
+    {
+        private double dolar, conversao;
+        //construtores
+        public Dolar() { this.dolar = 0; }
+        public Dolar(int dolar) { this.dolar = dolar; }
+        //set
+        public void SetReal(int dolar){ this.dolar = dolar; }
+        //get
+        public double GetConversao() {  return this.conversao; }
+        public double GetDolar() {  return this.dolar; }
+       
+        //resultado
+        public void Converte() { this.conversao = this.dolar/5; }
+        
+        
+    }
+}
---
+++ POO_Ex6/POO_Ex6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar dolar = new Dolar(100);  
+            dolar.Converte();
+            Console.WriteLine(dolar.GetConversao());
+        }
+    }
+}
