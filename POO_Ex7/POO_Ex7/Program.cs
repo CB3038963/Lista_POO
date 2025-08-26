4629f24920481f07e1a43b63a3fa9e6dc1723694
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-26 01:47:46 -0300

    Add files via upload

---
+++ POO_Ex7/POO_Ex7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior maior = new Maior(50,10);
+            maior.Verificar();
+            Console.WriteLine(maior.GetMaior());
+        }
+    }
+}
