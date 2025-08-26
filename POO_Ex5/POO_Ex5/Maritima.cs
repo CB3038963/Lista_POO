ea3ac037511f978896d279fd862b3364fe581020
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-26 01:24:09 -0300

    Add files via upload

---
+++ POO_Ex5/POO_Ex5/Maritima.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex5
+{
+    internal class Maritima
+    {
+        private double milhaM = 1852;
+        private double metro;
+        private double milha;
+        //construtores
+        public Maritima () { this.milha = 0; }
+        public Maritima (double milha) {  this.milha = milha; }
+        //set
+        public void SetMilha(double milha) {  this.milha=milha; }
+        //Get
+        public double GetMetro() { return this.metro; }
+        public double GetMilha() {  return this.milha; }    
+        
+        //resultado
+        public void Converter() { this.metro = this.milha * this.milhaM; }
+
+    }
+}
---
+++ POO_Ex5/POO_Ex5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maritima maritima = new Maritima(2);
+            maritima.Converter();
+            Console.WriteLine(maritima.GetMetro());
+        }
+    }
+}
