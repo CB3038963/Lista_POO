da1131157176570398395a911572e1edb48bb8cb
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-30 16:14:17 -0300

    Add files via upload

---
+++ POO_Ex10/POO_Ex10/Peso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex10
+{
+    internal class Peso
+    {
+        private double p, a, r;
+        //construtores
+        public Peso()
+        { this.p = 0; this.a = 0; }
+        public Peso(double a, double p)
+        { this.p = p; this.a = a; }
+        //set
+        public void SetAltura(double p){this.p=p; }
+        public void SetPeso(double p){this.p=p; }
+        //get
+        public double GetAltura() { return this.p; }
+        public double GetPeso() { return this.p; }      
+        public double GetResultado () { return this.r; }    
+        //calculando
+        public void calculo()
+        { this.r = this.p / (Math.Pow(this.a, 2)); }
+    }
+}
---
+++ POO_Ex10/POO_Ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Peso peso = new Peso(1.85,90);
+            peso.calculo();
+            if (peso.GetResultado() < 20) { Console.WriteLine("Abaixo do peso"); }
+            else if (peso.GetResultado() > 20 &&  peso.GetResultado() < 25) { Console.WriteLine("Peso ideal"); }
+            else { Console.WriteLine("Acima do peso"); }
+        }
+    }
+}
