3767a5dd31c394b3235b597c33778d7dc1b62aa9
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-30 15:46:21 -0300

    Add files via upload

---
+++ POO_Ex9/POO_Ex9/Calcular.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex9
+{
+    internal class Calcular
+    {
+        private double b, h, a;
+        //construtores
+        public Calcular () { b = 0; h = 0; }
+        public Calcular (double b, int h) { this.b = b; this.h = h; }
+        //set
+        public void SetBase(double b) { this.b = b; }
+        public void SetAltura(double h) { this.h = h; }
+        //get
+        public double GetBase() { return this.b; }
+        public double GetAltura () { return this.h; }   
+        public double GetArea () { return this.a; }
+        //calculando a area
+        public void calcular ()
+
+        { this.a = this.b * this.h; }
+
+    }
+}
---
+++ POO_Ex9/POO_Ex9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular calcular = new Calcular(10,10);
+            calcular.calcular();
+            if (calcular.GetArea() < 100) { Console.WriteLine("Terreno Pequeno"); }
+            else { Console.WriteLine("Terreno Grande"); }
+           
+        }
+    }
+}
