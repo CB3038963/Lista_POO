b4301fe991c92bb00afcb06bd4dcaa882eced72f
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-30 17:12:44 -0300

    Add files via upload

---
+++ POO_Ex12/POO_Ex12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo(4,3,5);
+            triangulo.calculo();
+        }
+    }
+}
