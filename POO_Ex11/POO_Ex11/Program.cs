452f11c0c106bbbd5b53b3788c1f2dbada2bcc93
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-30 16:54:02 -0300

    Add files via upload

---
+++ POO_Ex11/POO_Ex11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo(5,4,50);
+            triangulo.resolucao();
+            
+            
+            
+        }
+    }
+}
---
+++ POO_Ex11/POO_Ex11/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex11
+{
+    internal class Triangulo
+    {
+        private double a, b, c, r;
+        //construtores
+        public Triangulo()
+        { this.a = 0;this.b = 0;this.c = 0; }
+        public Triangulo(double a,double b,double c)
+        { this.a=a;this.b=b;this.c=c; } 
+        //set
+        public void SetA(double a) { this.a=a; }
+        public void SetB(double b) { this.b = b; }
+        public void SetC(double c) { this.c=c; }
+        //get
+        public double GetA (double a) { return this.a; }
+        public double GetB (double b) { return this.b; }
+        public double GetC (double c) { return this.c; }
+
+        //resultado
+
+        public void resolucao()
+        {
+            if (this.a < this.b + this.c && this.b < this.c + this.a && this.c < this.a + this.b)
+            {
+                if (this.a == this.b && this.a == this.c) { Console.WriteLine("Forma um triangulo Equilatero"); }
+                if (this.a != this.b && this.a != this.c && this.c != this.b) { Console.WriteLine("Forma um triangulo Escaleno"); }
+                if (this.a == this.b && this.a != this.c || this.b == this.c && this.c != this.a ||this.c == this.a && this.a != this.b) { Console.WriteLine("Forma um triangulo Isóscele"); }
+            }
+            else { Console.WriteLine("Não forma uma triangulo"); }
+        }
+    }
+}
