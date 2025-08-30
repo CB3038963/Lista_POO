ec6df064755146052a723242daa6cd3eb53931b0
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-08-30 15:54:35 -0300

    Add files via upload

---
+++ POO_Ex8/POO_Ex8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior maior = new Maior(10, 100);
+            maior.Verificar();
+            if (maior.GetMaior() > 0) { Console.WriteLine(maior.GetMaior()); }
+            else { Console.WriteLine("Os números são identicos"); }
+        }
+    }
+}
